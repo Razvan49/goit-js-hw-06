e96b892ab87e592d9d9eb044044a1caf15cee65d
Razvan49 <razvan_pavel87@yahoo.com>
2023-08-13 14:27:16 +0300

    task 09 completed

--- js/task-08.js
+++ js/task-08.js
@@ -20,3 +20,26 @@ Dacă formularul are câmpuri goale, afișați un alert care avertizează că to
 Dacă utilizatorul a completat toate câmpurile și a trimis formularul, colectați valorile câmpului într-un obiect în care numele câmpului va fi numele proprietății, iar valoarea câmpului va fi valoarea proprietății. Pentru a accesa elementele formularului, folosiți proprietatea elements.
 Afișați obiectul cu datele introduse în consolă și ștergeți valorile câmpurilor din formular, folosind metoda reset.
 */
+
+const form = document.querySelector(".login-form"); // accesam formularul
+console.log(form); // verificam accesarea
+
+// Adaugă un eveniment de ascultare pentru evenimentul "submit" pe form#login-form
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // anulam refresh-ul paginii
+  const { elements } = e.currentTarget;
+  const email = elements.email.value;
+  const password = elements.password.value;
+
+  if (!email || !password) {
+    console.log(`Email si parola sunt obligatorii!`);
+    alert("Email si parola sunt obligatorii!");
+  } else {
+    const formData = {
+      email: email,
+      password: password,
+    };
+    console.log(formData);
+  }
+  form.reset();
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,6 +1,7 @@
 /* 
 Exercițiul 9
-Scrieți un script care schimbă culorile de fundal al elementului <body>, prin stiluri inline, atunci când se face click pe button.change-color și afișați valoarea culorii în span.color.
+Scrieți un script care schimbă culorile de fundal al elementului <body>, prin stiluri inline, 
+atunci când se face click pe button.change-color și afișați valoarea culorii în span.color.
 
 <div class="widget">
   <p>Background color: <span class="color">-</span></p>
@@ -15,8 +16,22 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 */
+
+// Functia de generare culoare random
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorButton = document.querySelector(".change-color"); // accesam butonul
+const colorSpan = document.querySelector(".color"); //accesam cspanul care o sa afiseze culoarea ca text
+const body = document.body; // accesam body
+
+// Adaugă un eveniment de ascultare pentru evenimentul "click" pe changeColorButton.change-color]
+changeColorButton.addEventListener("click", () => {
+  const randomColor = getRandomHexColor(); // trimitem valorea culorii din functia getRamdomHexColor la randomColor
+
+  body.style.backgroundColor = randomColor; // actualizam culoarea body cu culoarea randomColor
+  colorSpan.textContent = randomColor; // actualizam spanul cu randomColor
+});
